9b37bec8e9f465bd681575ef5c74de3456949338
Tashaigg <99045602+Tashaigg@users.noreply.github.com>
2022-04-26 12:01:56 -0300

    Change cwd final to be MadLips/

--- inichecker.py
+++ inichecker.py
@@ -11,19 +11,18 @@ def inichecker():
         os.makedirs("docs")
     except:
         print("Welcome\nLet's have fun\n")
-    os.chdir(f"{Path.home()}/MadLips/docs")
 
-    ps1 = open("Story1.txt", "w")
+    ps1 = open("docs/Story1.txt", "w")
     ps1.write(
         "The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events."
     )
     ps1.close()
-    ps2 = open("Story2.txt", "w")
+    ps2 = open("docs/Story2.txt", "w")
     ps2.write(
         "NOUN declared that it's ilegal to VERB, NOUN did it anyway because they were ADJECTIVE."
     )
     ps2.close()
-    p = open("Your Tales.txt", "a")
+    p = open("docs/Your Tales.txt", "a")
     p.close()
 
 if __name__ == "__main__":
